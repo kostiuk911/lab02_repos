98b35bf4f995c821b8f032b1627d9daf6776d0c9
kostiuk911 <kostiukvitaliy043@gmail.com>
2022-10-06 21:24:46 +0300

    first master commit

---
+++ Project1/Source.cpp
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <math.h>
+#include <conio.h>
+using namespace std;
+int main() {
+    double a;
+    double z1;
+   //  double z2;
+    cout << "a="; cin >> a; cout << endl;
+    z1 = (sin(M_PI / 2) + (3 * a)) / (1 - sin((3 * a) - M_PI));
+   // z2 = atan((5 / 4) * M_PI) + ((3 / 2) * a);
+    cout << "z1= " << z1 << endl;
+   // cout << "z2= " << z2 << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+
+}
