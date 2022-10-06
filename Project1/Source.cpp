95a74afd4bdbfa513b154d0c71e2803665c9091d
kostiuk911 <kostiukvitaliy043@gmail.com>
2022-10-06 22:39:24 +0300

    first branch_02 commit

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -9,13 +9,13 @@
 using namespace std;
 int main() {
     double a;
-    double z1;
-   //  double z2;
+   // double z1;
+     double z2;
     cout << "a="; cin >> a; cout << endl;
-    z1 = (sin(M_PI / 2) + (3 * a)) / (1 - sin((3 * a) - M_PI));
-   // z2 = atan((5 / 4) * M_PI) + ((3 / 2) * a);
-    cout << "z1= " << z1 << endl;
-   // cout << "z2= " << z2 << endl;
+   // z1 = (sin(M_PI / 2) + (3 * a)) / (1 - sin((3 * a) - M_PI));
+    z2 = atan((5 / 4) * M_PI) + ((3 / 2) * a);
+   // cout << "z1= " << z1 << endl;
+    cout << "z2= " << z2 << endl;
 
 
 
